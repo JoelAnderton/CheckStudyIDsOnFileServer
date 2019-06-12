87f749a52e272985db25c6b669a8f5c0b99d3f57
Joel Anderton <JOA48@pitt.edu>
2019-06-12 15:51:36 -0400

    Add functions per phenotype to find StudyIDs in SQL that completed those phenotypes

--- CheckStudyIDsOnFileServer/CheckStudyIDsOnFileServer_remote.py
+++ CheckStudyIDsOnFileServer/CheckStudyIDsOnFileServer_remote.py
@@ -30,25 +30,207 @@ def sql_connection():
     return connection
 
 
-def studyIDs_in_SQL():
-    """Finds StudyIDs already imported in SQL"""
+def oom_studyIDs():
+    """Finds completed OOM StudyIDs"""
     connection = sql_connection()
     cur = connection.cursor()
 
     sqlcode = ('''  
-    SELECT StudyID
+    SELECT 
+     [StudyID]
+       
     FROM IndividualChecklistExported
-
+    WHERE [LipUltrasound] = 1
     ''')
     cur.execute(sqlcode)
     studyIDs_in_SQl_list = []
     for row in cur.fetchall():
-        studyIDs_in_SQl_list.append(row[0])
+        studyIDs_in_SQl_list.append(row[0:])
     return studyIDs_in_SQl_list
 
-def studyIDs_on_Server():
-    """Finds StudyIDs used on file server"""
-    pass
+
+def lipPhotos_studyIDs():
+    """Finds completed LipPhotos StudyIDs"""
+    connection = sql_connection()
+    cur = connection.cursor()
+
+    sqlcode = ('''  
+    SELECT 
+     [StudyID]
+       
+    FROM IndividualChecklistExported
+    WHERE [LipPhotos] = 1
+    ''')
+    cur.execute(sqlcode)
+    studyIDs_in_SQl_list = []
+    for row in cur.fetchall():
+        studyIDs_in_SQl_list.append(row[0:])
+    return studyIDs_in_SQl_list
+
+
+def lhfPhoto_studyIDs():
+    """Finds completed LHFPhoto StudyIDs"""
+    connection = sql_connection()
+    cur = connection.cursor()
+
+    sqlcode = ('''  
+    SELECT 
+     [StudyID]
+       
+    FROM IndividualChecklistExported
+    WHERE [LHFPhoto] = 1
+    ''')
+    cur.execute(sqlcode)
+    studyIDs_in_SQl_list = []
+    for row in cur.fetchall():
+        studyIDs_in_SQl_list.append(row[0:])
+    return studyIDs_in_SQl_list
+
+
+def iop_studyIDs():
+    """Finds completed IOP StudyIDs"""
+    connection = sql_connection()
+    cur = connection.cursor()
+
+    sqlcode = ('''  
+    SELECT 
+     [StudyID]
+       
+    FROM IndividualChecklistExported
+    WHERE [IntraoralPhotos] = 1
+    ''')
+    cur.execute(sqlcode)
+    studyIDs_in_SQl_list = []
+    for row in cur.fetchall():
+        studyIDs_in_SQl_list.append(row[0:])
+    return studyIDs_in_SQl_list
+
+def palateVideo_studyIDs():
+    """Finds completed PalateVideo StudyIDs"""
+    connection = sql_connection()
+    cur = connection.cursor()
+
+    sqlcode = ('''  
+    SELECT 
+     [StudyID]
+       
+    FROM IndividualChecklistExported
+    WHERE [PalateVideo] = 1
+    ''')
+    cur.execute(sqlcode)
+    studyIDs_in_SQl_list = []
+    for row in cur.fetchall():
+        studyIDs_in_SQl_list.append(row[0:])
+    return studyIDs_in_SQl_list
+
+def photos3D_studyIDs():
+    """Finds completed Photos3D StudyIDs"""
+    connection = sql_connection()
+    cur = connection.cursor()
+
+    sqlcode = ('''  
+    SELECT 
+     [StudyID]
+       
+    FROM IndividualChecklistExported
+    WHERE [Photos3D] = 1
+    ''')
+    cur.execute(sqlcode)
+    studyIDs_in_SQl_list = []
+    for row in cur.fetchall():
+        studyIDs_in_SQl_list.append(row[0:])
+    return studyIDs_in_SQl_list
+
+def dentalImpression_studyIDs():
+    """Finds completed DentalImpression StudyIDs"""
+    connection = sql_connection()
+    cur = connection.cursor()
+
+    sqlcode = ('''  
+    SELECT 
+     [StudyID]
+       
+    FROM IndividualChecklistExported
+    WHERE [DentalImpression] = 1
+    ''')
+    cur.execute(sqlcode)
+    studyIDs_in_SQl_list = []
+    for row in cur.fetchall():
+        studyIDs_in_SQl_list.append(row[0:])
+    return studyIDs_in_SQl_list
+
+
+def handScan_studyIDs():
+    """Finds completed HandScan StudyIDs"""
+    connection = sql_connection()
+    cur = connection.cursor()
+
+    sqlcode = ('''  
+    SELECT 
+     [StudyID]
+       
+    FROM IndividualChecklistExported
+    WHERE [HandScan] = 1
+    ''')
+    cur.execute(sqlcode)
+    studyIDs_in_SQl_list = []
+    for row in cur.fetchall():
+        studyIDs_in_SQl_list.append(row[0:])
+    return studyIDs_in_SQl_list
+
+def idVideo_studyIDs():
+    """Finds completed IDVideo StudyIDs"""
+    connection = sql_connection()
+    cur = connection.cursor()
+
+    sqlcode = ('''  
+    SELECT 
+     [StudyID]
+       
+    FROM IndividualChecklistExported
+    WHERE [IDVideo] = 1
+    ''')
+    cur.execute(sqlcode)
+    studyIDs_in_SQl_list = []
+    for row in cur.fetchall():
+        studyIDs_in_SQl_list.append(row[0:])
+    return studyIDs_in_SQl_list
+
+def stVideo_studyIDs():
+    """Finds completed STVideo StudyIDs"""
+    connection = sql_connection()
+    cur = connection.cursor()
+
+    sqlcode = ('''  
+    SELECT 
+     [StudyID]
+       
+    FROM IndividualChecklistExported
+    WHERE [STVideo] = 1
+    ''')
+    cur.execute(sqlcode)
+    studyIDs_in_SQl_list = []
+    for row in cur.fetchall():
+        studyIDs_in_SQl_list.append(row[0:])
+    return studyIDs_in_SQl_list
+
+def spVideo_studyIDs():
+    """Finds completed SPVideo StudyIDs"""
+    connection = sql_connection()
+    cur = connection.cursor()
+
+    sqlcode = ('''  
+    SELECT 
+     [StudyID]
+       
+    FROM IndividualChecklistExported
+    WHERE [SPVideo] = 1
+    ''')
+    cur.execute(sqlcode)
+    studyIDs_in_SQl_list = []
+    for row in cur.fetchall():
+        studyIDs_in_SQl_list.append(row[0:])
+    return studyIDs_in_SQl_list
 
 def main():
     studyID_list = studyIDs_in_SQL()
